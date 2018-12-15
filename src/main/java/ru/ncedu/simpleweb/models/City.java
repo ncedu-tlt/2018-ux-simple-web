1aa969ffadea862a323e0efe87ecc5837c7e4560
SergeyMerkulov <freedms@yandex.ru>
2018-12-15 18:53:43 +0400

    Create City.java

---
+++ src/main/java/ru/ncedu/simpleweb/models/City.java
@@ -0,0 +1,69 @@
+package ru.ncedu.simpleweb.models;
+
+public class City implements Cloneable {
+
+    private long id;
+	private String name;
+	private String phoneExtension;
+    private String description;
+    
+    public City(){
+
+	}
+
+	public City(long id){
+        	this.id = id;
+	}
+
+	public City(long id, String name, String phoneExtension, String description){
+        	this.id = id;
+		this.name = name;
+		this.phoneExtension = phoneExtension;
+		this.description = description;
+	}
+
+	public City (City source){
+        	this(source.getId(), source.getName(), source.getPhoneExtension(), source.getDescription());
+    	}
+
+	public long getId(){
+        	return id;
+	}
+
+	public void setId(long id){
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getPhoneExtension(){
+        	return phoneExtension;
+	}
+
+	public void setPhoneExtension(String phoneExtension) {
+		this.phoneExtension = phoneExtension;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof City) {
+			City city = (City) obj;
+			return city.getId() == id;
+		}
+		return false;
+	}
+}
