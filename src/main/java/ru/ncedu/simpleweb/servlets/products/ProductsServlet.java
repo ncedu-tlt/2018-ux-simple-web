f6b776973f242bc908d84b44e762668cb188ab6c
KhMaria <hvostova_masha@mail.ru>
2018-12-15 22:57:03 +0400

    Add products view

--- src/main/java/ru/ncedu/simpleweb/servlets/products/ProductsServlet.java
+++ src/main/java/ru/ncedu/simpleweb/servlets/products/ProductsServlet.java
@@ -12,16 +12,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(name = "products", urlPatterns = {"/products"})
 public class ProductsServlet extends HttpServlet {
     private static final String PRODUCTS_ATTR = "products";
+    private static final String CATEGORIES_ATTR = "categories";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         List<Product> products = ProductsRepository.getInstance().get();
+        List<Category> categories = new ArrayList<>();
+        for (Product product : products) {
+            Category category = CategoriesRepository.getInstance().get(product.getCategoryId());
+            categories.add(category);
+        }
+
         req.setAttribute(PRODUCTS_ATTR, products);
+        req.setAttribute(CATEGORIES_ATTR, categories );
 
         req.getRequestDispatcher(Views.PRODUCTS).forward(req, resp);
     }
