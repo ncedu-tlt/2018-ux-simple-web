d818b5e6e42d4eb6761e5dfba94587b6a7f1e0ac
KhMaria <hvostova_masha@mail.ru>
2018-12-24 17:17:47 +0400

    Change validation check and displaying error message

--- src/main/webapp/js/products.js
+++ src/main/webapp/js/products.js
@@ -1,28 +1,22 @@
-function isValidForm() {
+document.getElementById('form').onsubmit = function () {
     var productName = document.getElementById('productName').value;
 
     var categoryElement = document.getElementById('category');
-    var categoryName = categoryElement.options[categoryElement.selectedIndex].text;
+    var categoryName = categoryElement.options[categoryElement.selectedIndex].value;
 
     var productDescription = document.getElementById('productDescription').value;
 
-    var isValid = (productName !== "") && (categoryName !== "") && (productDescription !== "");
-
+    var isValid = productName && categoryName && productDescription;
     if(!isValid){
-        errorInfo("Please, fill up all fields before submitting the form");
+        showErrorMessage();
         return false;
     }
     else{
         return true;
     }
-}
+};
 
-function errorInfo(message){
-    var div = document.createElement('div');
-    div.className = "form__error";
-    div.innerHTML = message;
-
-    var errorElement = document.getElementById('error');
-    errorElement.appendChild(div);
+function showErrorMessage(){
+    document.getElementById('error').style.display = "block";
 }
 
