25ca13764323b1c2c46453a16f43b3b706380e1d
KhMaria <hvostova_masha@mail.ru>
2019-01-10 15:38:46 +0400

    Change query type for remove products

--- src/main/java/ru/ncedu/simpleweb/servlets/products/ProductsRemoveServlet.java
+++ src/main/java/ru/ncedu/simpleweb/servlets/products/ProductsRemoveServlet.java
@@ -8,42 +8,39 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import org.json.JSONObject;
-import ru.ncedu.simpleweb.consts.Views;
 import ru.ncedu.simpleweb.repositories.ProductsRepository;
 
 @WebServlet(name = "productsRemove", urlPatterns = {"/products/remove"})
-
 public class ProductsRemoveServlet extends HttpServlet {
 
     private static final String PRODUCT_ID_PARAM = "productId";
-    private static final String ERROR_ATTR = "error";
-
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String productIdStr = request.getParameter(PRODUCT_ID_PARAM);
-
-        if(productIdStr == null || productIdStr.isEmpty()) {
-            request.setAttribute(ERROR_ATTR, true);
-            request.getRequestDispatcher(Views.PRODUCTS).forward(request, response);
-            return;
-        }
-        long productId = Long.parseLong(request.getParameter(PRODUCT_ID_PARAM));
-        boolean success = ProductsRepository.getInstance().remove(productId);
         response.setContentType("application/json");
         response.setCharacterEncoding("utf-8");
+
+        if(productIdStr == null || productIdStr.isEmpty()) {
+            try (PrintWriter out = response.getWriter()) {
+                JSONObject jsonEnt = new JSONObject();
+                jsonEnt.put("serverInfo", "fail");
+                out.print(jsonEnt.toString());
+            }
+            return;
+        }
+
+        long productId = Long.parseLong(request.getParameter(PRODUCT_ID_PARAM));
+        boolean success = ProductsRepository.getInstance().remove(productId);
         try (PrintWriter out = response.getWriter()) {
             JSONObject jsonEnt = new JSONObject();
             if (success) {
                 jsonEnt.put("serverInfo", "success");
-                jsonEnt.put("productId", productId);
             } else {
-                jsonEnt.put("serverInfo", "failServlet");
-                jsonEnt.put("productId", productId);
+                jsonEnt.put("serverInfo", "fail");
             }
             out.print(jsonEnt.toString());
         }
-
     }
+
 }
