4b745cf6f1a2b8a2681e99d7c529efecdb026951
Юсупов Наиль <nail1999jusupov@gmail.com>
2019-02-07 14:13:57 +0400

    Made delete country servlet

---
+++ src/main/java/ru/ncedu/simpleweb/servlets/countries/CountriesRemoveServlet.java
@@ -0,0 +1,50 @@
+package ru.ncedu.simpleweb.servlets.countries;
+
+import ru.ncedu.simpleweb.repositories.CountriesRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.json.JSONObject;
+
+@WebServlet(name = "countryRemove", urlPatterns = {"/countries/remove"})
+public class CountriesRemoveServlet extends HttpServlet {
+
+    private static final String COUNTRY_ID_ATTR = "id";
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        String countryId = req.getParameter(COUNTRY_ID_ATTR);
+
+        res.setContentType("application/json");
+        res.setCharacterEncoding("utf-8");
+
+        if (countryId == null || countryId.isEmpty()) {
+            try (PrintWriter out = res.getWriter()) {
+                JSONObject jsonEnt = new JSONObject();
+                jsonEnt.put("isSuccess", false);
+                out.print(jsonEnt.toString());
+            }
+            return;
+        }
+
+
+        long countryIdNumber = Long.parseLong(countryId);
+        boolean isDeleted = CountriesRepository.getInstance().removeById(countryIdNumber);
+        try (PrintWriter out = res.getWriter()) {
+            JSONObject jsonEnt = new JSONObject();
+            if (isDeleted) {
+                jsonEnt.put("isSuccess", true);
+            } else {
+                jsonEnt.put("isSuccess", false);
+            }
+            out.print(jsonEnt.toString());
+        }
+    }
+}
